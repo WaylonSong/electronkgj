357cb25ee78564ab84b9aa9e9833fd0d4def03df
WaylonFocus <songwenlue@gmail.com>
2018-01-11 11:22:23 +0800

    edit short-cuts

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var electron = require('electron') // http://electron.atom.io/docs/api
 var path = require('path')         // https://nodejs.org/api/path.html
 var url = require('url')           // https://nodejs.org/api/url.html
 var ipc = require('node-ipc')
-const { dialog } = require('electron');
+const { dialog, Menu } = require('electron');
 var window = null
 var util = require('./components/util')
 var ipcMain = electron.ipcMain;
@@ -20,8 +20,117 @@ ipcMain.on('export',function(event, arg0){
         // require('child_process').exec(command); 
     })
 })
+
+var menuArray = [
+    {
+        label: 'Application',
+        submenu: [
+            {
+                label: 'About Hosts High',
+                click: function() {
+                    new BrowserWindow({
+                        parent: BrowserWindow.getFocusedWindow(),
+                        width: 285,
+                        height: 230,
+                        center: true,
+                        resizable: false,
+                        minimizable: false,
+                        maximizable: false,
+                        show: true,
+                        title: ''
+                    })
+                        .loadURL('file://' + resolve(__dirname, 'about.html'));
+                }
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Quit Hosts High',
+                accelerator: 'CmdOrCtrl+Q',
+                click: function() {
+                    app.quit();
+                }
+            }
+        ]
+    },
+    {
+        label: 'Edit',
+        submenu: [
+            {
+                label: 'Undo',
+                accelerator: 'CmdOrCtrl+Z',
+                selector: 'undo:',
+                role: 'undo'
+            },
+            {
+                label: 'Redo',
+                accelerator: 'Shift+CmdOrCtrl+Z',
+                selector: 'redo:',
+                role: 'redo'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Cut',
+                accelerator: 'CmdOrCtrl+X',
+                selector: 'cut:',
+                role: 'cut'
+            },
+            {
+                label: 'Copy',
+                accelerator: 'CmdOrCtrl+C',
+                selector: 'copy:',
+                role: 'copy'
+            },
+            {
+                label: 'Paste',
+                accelerator: 'CmdOrCtrl+V',
+                selector: 'paste:',
+                role: 'paste'
+            },
+            {
+                label: 'Select All',
+                accelerator: 'CmdOrCtrl+A',
+                selector: 'selectAll:',
+                role: 'selectall'
+            }
+        ]
+    },
+    {
+        label: 'View',
+        submenu: [
+            {
+                label: 'Reload',
+                accelerator: 'CmdOrCtrl+R',
+                click: function(item, focusedWindow) {
+                    if (focusedWindow) {
+                        focusedWindow.reload();
+                    }
+                }
+            },
+            {
+                label: 'Toggle Developer Tools',
+                accelerator: (function() {
+                    if (process.platform === 'darwin') {
+                        return 'Alt+Command+I';
+                    }
+                    return 'Ctrl+Shift+I';
+                }()),
+                click: function(item, focusedWindow) {
+                    if (focusedWindow) {
+                        focusedWindow.toggleDevTools();
+                    }
+                }
+            }
+        ]
+    }
+];
 // Wait until the app is ready
 electron.app.once('ready', function () {
+  Menu.setApplicationMenu(Menu.buildFromTemplate(menuArray));
+  console.log(menuArray)
   // Create a new window
   window = new electron.BrowserWindow({
     // Set the initial width to 800px
